0b238ebd603e4a2db8ad0b35312708277d7b4674
bneil <joaoferreira.ac23@gmail.com>
2025-07-01 16:52:45 +0100

    Adding Normal Items

--- src/main/java/net/bneil/skydungeon/SkyDungeon.java
+++ src/main/java/net/bneil/skydungeon/SkyDungeon.java
@@ -1,5 +1,6 @@
 package net.bneil.skydungeon;
 
+import net.bneil.skydungeon.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -53,6 +54,8 @@ public class SkyDungeon {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -66,6 +69,10 @@ public class SkyDungeon {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.BISMUTH);
+            event.accept(ModItems.RAW_BISMUTH);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
