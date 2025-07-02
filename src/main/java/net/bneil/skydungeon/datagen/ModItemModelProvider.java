10612acefff3cc3e5bae7cec6996e77c6de1d44c
bneil <joaoferreira.ac23@gmail.com>
2025-07-02 17:50:14 +0100

    Adding DataGen

---
+++ src/main/java/net/bneil/skydungeon/datagen/ModItemModelProvider.java
@@ -0,0 +1,25 @@
+package net.bneil.skydungeon.datagen;
+
+
+import net.bneil.skydungeon.SkyDungeon;
+import net.bneil.skydungeon.item.ModItems;
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.client.model.generators.ItemModelProvider;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+
+public class ModItemModelProvider extends ItemModelProvider {
+    public ModItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
+        super(output, SkyDungeon.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+        basicItem(ModItems.BISMUTH.get());
+        basicItem(ModItems.RAW_BISMUTH.get());
+
+        basicItem(ModItems.RADISH.get());
+        basicItem(ModItems.STARLIGHT_ASHES.get());
+        basicItem(ModItems.FROSTFIRE_ICE.get());
+        basicItem(ModItems.CHISEL.get());
+    }
+}
